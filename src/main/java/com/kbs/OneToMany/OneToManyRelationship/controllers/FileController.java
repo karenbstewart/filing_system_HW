8cd6de5d3821a1cefd164df6305e9cb1c1db98a5
Karen Stewart <kastewart2002@googlemail.com>
2021-12-01 01:14:24 +0000

    file controllers added

--- src/main/java/com/kbs/OneToMany/OneToManyRelationship/controllers/FileController.java
+++ src/main/java/com/kbs/OneToMany/OneToManyRelationship/controllers/FileController.java
@@ -1,4 +1,35 @@
 package com.kbs.OneToMany.OneToManyRelationship.controllers;
 
+import com.kbs.OneToMany.OneToManyRelationship.models.File;
+import com.kbs.OneToMany.OneToManyRelationship.models.Folder;
+import com.kbs.OneToMany.OneToManyRelationship.repositories.FileRepository;
+import com.kbs.OneToMany.OneToManyRelationship.repositories.FolderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class FileController {
+
+    @Autowired
+    FileRepository fileRepository;
+
+    @GetMapping(value = "/files")
+    public ResponseEntity<List<File>> getAllFile(){
+        return new ResponseEntity<>(fileRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/files/{id}")
+    public ResponseEntity getFile(@PathVariable Long id){
+        return new ResponseEntity(fileRepository.findById(id), HttpStatus.OK) ;
+    }
+
+    @PostMapping(value = "/files")
+    public ResponseEntity<File> postFile(@RequestBody File file){
+        fileRepository.save(file);
+        return new ResponseEntity<>(file, HttpStatus.CREATED);
+    }
 }
